d1fbab03e841b38f8db7698ed49717f6147f8fd9
SaturKub <saturgamer.pinto@gmail.com>
2023-04-04 21:55:18 +0700

    E
    now i fuc pete

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,45 @@
-const json = [
-    {"id": 1, "message": "nice"},
-    {"id": 2, "message": "yay"},
-    {"id": 3, "message": "ez"}
-]
-for 
+import { Client, GatewayIntentBits, Partials ,Message} from "discord.js";
+import * as dotenv from "dotenv";
+import * as process from "node:process";
+
+
+(async () => {
+    const client = new Client({
+        intents: [
+            GatewayIntentBits.Guilds,
+            GatewayIntentBits.GuildMembers,
+            GatewayIntentBits.GuildMessages,
+            GatewayIntentBits.GuildMessageReactions,
+            GatewayIntentBits.DirectMessages,
+            GatewayIntentBits.DirectMessageReactions,
+            GatewayIntentBits.GuildIntegrations,
+            GatewayIntentBits.GuildPresences,
+            GatewayIntentBits.MessageContent
+        ],
+        partials: [
+            Partials.Channel,
+            Partials.GuildMember,
+            Partials.Message,
+            Partials.Reaction,
+            Partials.User
+        ],
+    });
+
+    dotenv.config();
+
+    const prefix = "beaw!";
+
+    async function onMessageCreate(message: Message) {
+        if (message.content.startsWith(prefix)) {
+            if (message.content === prefix + "pete") {
+                const tag = "<@" + "749512547144564757" + ">";
+                await message.channel.send(tag);
+            }
+        }
+    }
+    client.on("messageCreate", onMessageCreate);
+    function onLogin(){
+        console.log("Bot is still ofline :)");
+    }
+    await client.login(process.env["TOKEN"]).then(onLogin);
+})();
